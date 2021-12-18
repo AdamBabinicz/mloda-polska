fa9bfa4c381d3d3d81c130472e627f1cbc0d5e8b
babinicz <babinicz@onet.pl>
2021-12-18 20:11:04 +0100

    zm

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -29,6 +29,9 @@ const AboutStyles = styled.div`
     font-size: 1.6rem;
     line-height: 1.7;
   }
+  .about__info {
+    margin-left: 3rem;
+  }
   @media only screen and (max-width: 768px) {
     padding: 1rem 0;
     .about__wrapper {
